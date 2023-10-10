9087fd9881e5140fe1f332ca4793782313aa5c82
Kush <bhattkush2009@gmail.com>
2023-10-09 21:49:00 -0700

    Custom grid settings

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 let container = document.getElementById('container');
-let counter = 1;
-for(i=1; i<=4; i++){
-    for(j=1; j<=4; j++){
-        let newdiv = document.createElement('div');
-        newdiv.setAttribute('id', `${counter}`)
-        container.appendChild(newdiv);
-        counter++;
-        newdiv.addEventListener('hover', function(){
-            document.getElementById(`${counter}`).style.backgroundColor = 'blue';
-        })
+let newgrid = document.getElementById('newgrid')
+newgrid.addEventListener('click', function(){
+    let gridInput = document.getElementById('gridsize');
+    let counter = 1;
+    let gridSize = parseInt(gridInput.value);
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
     }
-}
-let divs = container.children;
-document.addEventListener('')
+    for(i=1; i<=gridSize; i++){
+        for(j=1; j<=gridSize; j++){
+            let newdiv = document.createElement('div');
+            newdiv.setAttribute('id', `${counter}`);
+            newdiv.classList.add("grid-item");
+            container.appendChild(newdiv);
+            counter++;
+            newdiv.addEventListener('mouseenter', function(){
+                this.style.backgroundColor = 'blue';
+            })
+        }
+    }
+    
+});
