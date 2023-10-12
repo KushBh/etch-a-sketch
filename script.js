5202a0fd228da407a08746a7d69076b807dcc8a4
Kush <bhattkush2009@gmail.com>
2023-10-11 21:58:57 -0700

    debugged

--- script.js
+++ script.js
@@ -2,22 +2,16 @@ let container = document.getElementById('container');
 let newgrid = document.getElementById('newgrid')
 newgrid.addEventListener('click', function(){
     let gridInput = document.getElementById('gridsize');
-    let counter = 1;
     let gridSize = parseInt(gridInput.value);
-    while(container.firstChild){
-        container.removeChild(container.firstChild);
-    }
+    container.innerHTML('');
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     for(i=1; i<=gridSize; i++){
-        for(j=1; j<=gridSize; j++){
-            let newdiv = document.createElement('div');
-            newdiv.setAttribute('id', `${counter}`);
-            newdiv.classList.add("grid-item");
-            container.appendChild(newdiv);
-            counter++;
-            newdiv.addEventListener('mouseenter', function(){
-                this.style.backgroundColor = 'blue';
-            })
-        }
+        let newdiv = document.createElement('div');
+        newdiv.classList.add("grid-item");
+        container.appendChild(newdiv);
+        newdiv.addEventListener('mouseenter', function(){
+            this.style.backgroundColor = 'blue';
+        })
     }
     
 });
